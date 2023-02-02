5da2c5d7c1ee7ffe9a3b21dabc303086175d01e8
Solodkov <cartmaness045@gmail.com>
2023-02-02 23:20:42 +0500

    commit2

---
+++ HW1/script.js
@@ -0,0 +1,3 @@
+let tempcher = 50;
+let farengeit = (9/5)*tempcher+32;
+console.log(`${tempcher}°C = ${farengeit}°F`);
--- HW1/scrypt.js
+++
@@ -1,3 +0,0 @@
-let tempcher = 5;
-let farengeit = (9/5)*tempcher+32;
-console.log(`${tempcher}°C = ${faengeit}°F`)
